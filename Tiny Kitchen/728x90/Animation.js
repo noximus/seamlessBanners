7036753f2fcc2eb3f4db8f5c2566fd1ebf44f75c
schao87 <schaosti@gmail.com>
2015-10-15 15:11:12 -0400

    editted animation timing

--- Tiny Kitchen/728x90/Animation.js
+++ Tiny Kitchen/728x90/Animation.js
@@ -26,9 +26,11 @@ var BanTimeline = {
     });
     document.getElementById("container").style.display = "block";
     // initial settings for banner frame 1.  In case banner needs to restart.  add as many as you can to CSS first so Banner doesn't jump
-      TweenLite.to(your, 0, {scaleX:0, scaleY:0});
-      TweenLite.to(f5OrderNow, 0, {scaleX:1.5, scaleY:1.5});
+      TweenLite.to(your, 0, {left:-250,scaleX:0, scaleY:0});
+      TweenLite.to(f5OrderNow, 0, {top:50,scaleX:1, scaleY:1.5});
       TweenLite.to(f4Subletting, 0, {left:600, opacity:0, display:'none'});
+      TweenLite.to(f5Logo, 0, {left:-100});
+      TweenLite.to(f5NyEats, 0, {left:-100});
     // run animation after init is done. 
     BanTimeline.frame1();
   },
@@ -36,16 +38,16 @@ var BanTimeline = {
     
   },
   frame1: function () {
-    TweenLite.to(your, .2, {opacity:1, display: 'block', scaleX:1, scaleY:1, delay:.2, ease:Power1.easeOut});
-    TweenLite.to(tinyKitchen, .2, {opacity:1, display: 'block', delay:.3, ease:Power1.easeOut});
+    TweenLite.to(your, .1, {opacity:1, display: 'block',left:0,scaleX:1, scaleY:1, delay:.2, ease:Power1.easeOut});
+    TweenLite.to(tinyKitchen, .2, {opacity:1, display: 'block', delay:.5, ease:Power1.easeOut});
     TweenLite.delayedCall(3.5, BanTimeline.frame2);
   },
   frame2: function () {
     TweenLite.to(frame1Wrap, .2, {opacity:0, display: 'none', delay: 0, ease:Power4.easeOut});
     TweenLite.to(f2willThankYou, .2, {opacity:1, display: 'block', delay:.4, ease:Power1.easeOut});
-    TweenLite.to(f2Star1, .2, {display:'block', opacity:1, delay:.6, ease:Power4.easeOut});
-    TweenLite.to(f2Star2, .2, {display:'block', opacity:1, delay:.7, ease:Power4.easeOut});
-    TweenLite.to(f2Star3, .5, {display:'block', opacity:1, delay:.8, ease:Power4.easeOut});
+    TweenLite.to(f2Star1, .2, {display:'block', opacity:1, delay:.8, ease:Power4.easeOut});
+    TweenLite.to(f2Star2, .2, {display:'block', opacity:1, delay:.9, ease:Power4.easeOut});
+    TweenLite.to(f2Star3, .5, {display:'block', opacity:1, delay:1, ease:Power4.easeOut});
 
     TweenLite.delayedCall(4, BanTimeline.frame3);
   },
@@ -67,9 +69,7 @@ var BanTimeline = {
     TweenLite.to(frame4Wrap, .2, {opacity:0, display: 'none', delay: 0, ease:Power4.easeOut});
     TweenLite.to(f5Logo, .4, {opacity:1, display: 'block', delay: .3, ease:Power4.easeOut});
     TweenLite.to(f5NyEats, .4, {opacity:1, display: 'block', delay: .8, ease:Power4.easeOut});
-    TweenLite.to(f5Logo, .5, {left:-100, delay: 1.3, ease:Power4.easeOut});
-    TweenLite.to(f5NyEats, .5, {left:-100, delay: 1.3, ease:Power4.easeOut});
-    TweenLite.to(f5OrderNow, .5, {opacity:1, display: 'block', scaleX:1, scaleY:1, delay: 1.3, ease:Power4.easeOut});
+    TweenLite.to(f5OrderNow, .5, {opacity:1, display: 'block',top:0, scaleX:1, scaleY:1, delay: 1.3, ease:Power4.easeOut});
   }
 };
 window.onload = function() {if (Enabler.isInitialized()) {enablerInitHandler();} else {Enabler.addEventListener(studio.events.StudioEvent.INIT,enablerInitHandler);}}
