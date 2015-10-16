7e4ca93008efaaa6f2c19c03658ac97d0cef7c6e
schao87 <schaosti@gmail.com>
2015-10-16 17:49:55 -0400

    built 300x600

--- Your Secret/300x600/Animation.js
+++ Your Secret/300x600/Animation.js
@@ -14,33 +14,24 @@ var home_animation = {
   frame1: function () {
     // == show stars ==
     var i;
-    for (i = 1; i < 8; i++) {
+    for (i = 1; i < 13; i++) {
       var varNew = document.getElementById("star_"+i);
-      TweenLite.to("star_"+i, .5, {opacity:1, delay: .2*i, ease: Expo.easeOut});
+      TweenLite.to("star_"+i, .5, {opacity:1, delay: .1*i, ease: Expo.easeOut});
     }
     TweenLite.to(moon, 0.2, {opacity:1, delay:.8});
     TweenLite.to(building_color, 0.2, {opacity:1, delay:1.2});
     TweenLite.to(your, 0.1, {opacity:1, delay:1.4});
-    TweenLite.to(secret, 0.1, {opacity:1, delay:1.8});
-    TweenLite.to(ThreeAM, 0.1, {opacity:1, delay:2.4});
-    TweenLite.to(dumpling, 0.1, {opacity:1, delay:2.4});
-    TweenLite.to(and, 0.1, {opacity:1, delay:2.8});  
-    TweenLite.to(donuts, 0.1, {opacity:1, delay:3.2});
-    TweenLite.to(order, 0.1, {opacity:1, delay:3.2});
+    TweenLite.to(secret, 0.1, {opacity:1, delay:1.6});
 
-    TweenLite.delayedCall(5, home_animation.frame2);
+    TweenLite.delayedCall(3.5, home_animation.frame2);
   },
   frame2: function () {
     TweenLite.to(building, 0.1, {opacity:0, delay:0});
     TweenLite.to(building_color, 0.1, {opacity:0, delay:0});
     TweenLite.to(your, 0.1, {opacity:0, delay:0});
     TweenLite.to(secret, 0.1, {opacity:0, delay:0});
-    TweenLite.to(moon, 0.1, {opacity:0, delay:0});
-    TweenLite.to(ThreeAM, 0.1, {opacity:0, delay:0});
-    TweenLite.to(dumpling, 0.1, {opacity:0, delay:0});
-    TweenLite.to(and, 0.1, {opacity:0, delay:0});  
+    TweenLite.to(moon, 0.1, {opacity:0, delay:0});  
     TweenLite.to(donuts, 0.1, {opacity:0, delay:0});
-    TweenLite.to(order, 0.1, {opacity:0, delay:0});
     TweenLite.to(star_1, 0.1, {opacity:0, delay:0});
     TweenLite.to(star_2, 0.1, {opacity:0, delay:0});
     TweenLite.to(star_3, 0.1, {opacity:0, delay:0});
@@ -48,14 +39,43 @@ var home_animation = {
     TweenLite.to(star_5, 0.1, {opacity:0, delay:0});
     TweenLite.to(star_6, 0.1, {opacity:0, delay:0});
     TweenLite.to(star_7, 0.1, {opacity:0, delay:0});
+    TweenLite.to(star_8, 0.1, {opacity:0, delay:0});
+    TweenLite.to(star_9, 0.1, {opacity:0, delay:0});
+    TweenLite.to(star_10, 0.1, {opacity:0, delay:0});
+    TweenLite.to(star_11, 0.1, {opacity:0, delay:0});
+    TweenLite.to(star_12, 0.1, {opacity:0, delay:0});
 
     TweenLite.delayedCall(0.2, home_animation.frame3);
   },
   frame3: function () {
+    TweenLite.to(ThreeAM, 0.1, {opacity:1, delay:0});
+    TweenLite.to(dumpling, 0.1, {opacity:1, delay:0.3});
+    TweenLite.to(and, 0.1, {opacity:1, delay:.6}); 
+    TweenLite.to(donuts, 0.1, {opacity:1, delay:.9});
+    TweenLite.to(order, 0.1, {opacity:1, delay:1.2}); 
+    
+    TweenLite.delayedCall(3, home_animation.frame4);
+  },
+  frame4: function () {
+    TweenLite.to(ThreeAM, 0.1, {opacity:0, delay:0});
+    TweenLite.to(and, 0.1, {opacity:0, delay:0});  
+    TweenLite.to(donuts, 0.1, {opacity:0, delay:0});
+    TweenLite.to(dumpling, 0.1, {opacity:0, delay:0});
+    TweenLite.to(order, 0.1, {opacity:0, delay:0});
+
+    TweenLite.delayedCall(0.2, home_animation.frame5);
+  },
+  frame5: function () {
     TweenLite.to(safe, 0.4, {opacity:1, scaleX:1, scaleY:1});
-    TweenLite.to(logo, 0.3, {opacity:1, delay:2.7});
-    TweenLite.to(how, 0.3, {opacity:1, delay:3});
-    TweenLite.to(order_now, 0.3, {opacity:1, top:0, delay:3.2, scaleX:1, scaleY:1});
+
+    TweenLite.delayedCall(2.1, home_animation.frame6);
+  },
+  frame6: function () {
+    TweenLite.to(safe, 0.1, {opacity:0});
+    TweenLite.to(logo, 0.3, {opacity:1, delay:.5});
+    TweenLite.to(how, 0.3, {opacity:1, delay:.7});
+    TweenLite.to(order_now, 0.3, {opacity:1, top:0, delay:.9, scaleX:1, scaleY:1});
+
   }
 };
 // If true, start function. If false, listen for INIT.
